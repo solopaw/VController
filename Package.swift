a77152f1b7cba77adc3b4879ab88d75f7d981bc7
Adam Fraser <41173209+solopaw@users.noreply.github.com>
2022-04-04 12:10:02 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Charts",
+    name: "VideoGameController",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        .library(name: "Charts", targets: ["Charts"])
+        .library(name: "VideoGameController", targets: ["Source"])
     ],
     dependencies: [
-        .package(name: "Shapes", url: "https://github.com/spacenation/swiftui-shapes.git", .upToNextMajor(from: "1.1.0"))
+       
     ],
     targets: [
-        .target(name: "Charts",dependencies: ["Shapes"])
+        .target(name: "VideoGameController")
     ]
 )
